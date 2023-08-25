cf08ad11d130768b852ef0df15f5aaddae983d6b
Diego Herrera <diegoherreraruano91@gmail.com>
2023-08-25 02:08:24 -0600

    Co-authored-by: bryan-apg <bryan-apg@users.noreply.github.com> Co-authored-by: CodingWithGabe <CodingWithGabe@users.noreply.github.com>

---
+++ celularesVideoJuegos/celulares_videojuegos/src/Components/App.jsx
@@ -0,0 +1,13 @@
+import "../App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header>
+        <h1 className="title">🎬Peliculas</h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
